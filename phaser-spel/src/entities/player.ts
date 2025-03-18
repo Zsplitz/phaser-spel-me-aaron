d9fa4752d244da07ecad964280831cf927422fa5
Zsplitz <tallgren.zacharias@gamil.com>
2025-03-18 14:35:54 +0200

    liti ov level 1

--- phaser-spel/src/entities/player.ts
+++ phaser-spel/src/entities/player.ts
@@ -1,15 +1,23 @@
 import Phaser from "phaser";
 
 export default class Player extends Phaser.GameObjects.Rectangle {
-    private speed: number = 300;
-    private jumpStrength: number = 400;
+    private walkSpeed: number = 270; // Normal walking speed
+    private runSpeed: number = 450; // Increased speed when running
+    private jumpStrength: number = 550;
+    private acceleration: number = 200;
     private jumpKey: Phaser.Input.Keyboard.Key;
+    private leftKey: Phaser.Input.Keyboard.Key;
+    private rightKey: Phaser.Input.Keyboard.Key;
+    private runKey: Phaser.Input.Keyboard.Key;
     private body!: Phaser.Physics.Arcade.Body;
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 40, 40, 0x2277ff);
 
-        this.jumpKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.jumpKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.leftKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rightKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.runKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -19,8 +27,27 @@ export default class Player extends Phaser.GameObjects.Rectangle {
     }
 
     update(): void {
-        this.body.setVelocityX(this.speed);
+        // Default movement speed is walking speed
+        let moveSpeed = this.walkSpeed;
 
+        // If the run key (B) is held, increase speed
+        if (this.runKey.isDown) {
+            moveSpeed = this.runSpeed;
+        }
+
+        // Stop movement by default
+        this.body.setVelocityX(0);
+
+        // Left movement
+        if (this.leftKey.isDown) {
+            this.body.setVelocityX(-moveSpeed); // Use moveSpeed, not acceleration
+        }
+        // Right movement
+        else if (this.rightKey.isDown) {
+            this.body.setVelocityX(moveSpeed); // Use moveSpeed, not acceleration
+        }
+
+        // Jumping
         if (this.jumpKey.isDown && this.body.blocked.down) {
             this.body.setVelocityY(-this.jumpStrength);
         }
--- phaser-spel/src/scenes/level-1.ts
+++ phaser-spel/src/scenes/level-1.ts
@@ -16,9 +16,15 @@ export default class Level1Scene extends Phaser.Scene {
         this.player = new Player(this, 0, 480);
 
         this.platforms = this.physics.add.staticGroup();
-        new Platform(this, 0, 500, 600, 40, this.platforms);
-        new Platform(this, 750, 500, 1000, 40, this.platforms);
-        new Platform(this, 1400, 500, 1000, 40, this.platforms);
+        new Platform(this, 0, 500, 5000, 100, this.platforms);
+        new Platform(this, 800, 350, 40, 40, this.platforms);
+        new Platform(this, 950, 350, 200, 40, this.platforms);
+        new Platform(this, 1025, 200, 40, 40, this.platforms);
+        new Platform(this, 1280, 420, 80, 100, this.platforms);
+        new Platform(this, 1820, 370, 80, 200, this.platforms);
+        new Platform(this, 2400, 350, 80, 200, this.platforms);
+
+
 
         this.physics.add.collider(this.player, this.platforms);
     }
